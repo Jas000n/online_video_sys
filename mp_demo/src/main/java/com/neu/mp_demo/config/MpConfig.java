7410e733d50ff50b8eb10e4f35b2873b02aaa1f3
Jas000n <77543568+Jas000n@users.noreply.github.com>
2023-02-24 17:57:06 +0800

    2.24 learn mp 分页查询： //1.配置分页插件     @Bean     public PaginationInterceptor paginationInterceptor(){         return new PaginationInterceptor();     } //2.编写代码 				//创建page对象         Page<User> page = new Page<>(1,3);         //调用mp分页查询，会把分页数据封装到page对象里         usermapper.selectPage(page,null);         //通过page对象获取分页数据         System.out.println(page.getCurrent());//当前页         System.out.println(page.getRecords());//每页数据list集合         System.out.println(page.getSize());//每页显示记录数         System.out.println(page.getTotal());//总记录数         System.out.println(page.getPages());//总页数

--- mp_demo/src/main/java/com/neu/mp_demo/config/MpConfig.java
+++ mp_demo/src/main/java/com/neu/mp_demo/config/MpConfig.java
@@ -1,6 +1,7 @@
 package com.neu.mp_demo.config;
 
 import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +14,14 @@ import org.springframework.context.annotation.Configuration;
 @MapperScan("com.neu.mp_demo.mapper")
 public class MpConfig {
 
+    //乐观锁插件
     @Bean
     public OptimisticLockerInterceptor optimisticLockerInterceptor(){
         return new OptimisticLockerInterceptor();
+    }
+    //分页查询插件
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        return new PaginationInterceptor();
     }
 }
--- mp_demo/src/test/java/com/neu/mp_demo/MpDemoApplicationTests.java
+++ mp_demo/src/test/java/com/neu/mp_demo/MpDemoApplicationTests.java
@@ -1,11 +1,13 @@
 package com.neu.mp_demo;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.neu.mp_demo.entity.User;
 import com.neu.mp_demo.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
 import java.util.List;
 
 @SpringBootTest
@@ -50,6 +52,46 @@ public class MpDemoApplicationTests {
         int result = usermapper.updateById(test_user);
         System.out.println("result is : "+result);
 
+    }
+//    public static void main(String[] args) {
+//        System.out.println("控制台颜色测试：");
+//        System.out.println(getFormatLogString("[ 红色 ]", 31, 0));
+//        System.out.println(getFormatLogString("[ 黄色 ]", 32, 0));
+//        System.out.println(getFormatLogString("[ 橙色 ]", 33, 0));
+//        System.out.println(getFormatLogString("[ 蓝色 ]", 34, 0));
+//        System.out.println(getFormatLogString("[ 紫色 ]", 35, 0));
+//        System.out.println(getFormatLogString("[ 绿色 ]", 36, 0));
+//    }
+    //控制台带颜色
+    private static String getFormatLogString(String content, int color, int type) {
+        boolean hasType = type != 1 && type != 3 && type != 4;
+        if (hasType) {
+            return String.format("\033[%dm%s\033[0m", color, content);
+        } else {
+            return String.format("\033[%d;%dm%s\033[0m", color, type, content);
+        }
+    }
+
+    @Test
+    public void test_batch_select(){
+        //批量查询
+        List<User> users = usermapper.selectBatchIds(Arrays.asList(1L,2L,3L));
+        System.out.println(getFormatLogString(users.toString(),31,0));
+
+    }
+
+    @Test
+    public void testPage(){
+        //创建page对象
+        Page<User> page = new Page<>(1,3);
+        //调用mp分页查询，会把分页数据封装到page对象里
+        usermapper.selectPage(page,null);
+        //通过page对象获取分页数据
+        System.out.println(page.getCurrent());//当前页
+        System.out.println(page.getRecords());//每页数据list集合
+        System.out.println(page.getSize());//每页显示记录数
+        System.out.println(page.getTotal());//总记录数
+        System.out.println(page.getPages());//总页数
     }
 
 
