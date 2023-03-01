73a777452eb517605a1816de1ed02ffdf02a3474
Jas000n <77543568+Jas000n@users.noreply.github.com>
2023-03-01 09:16:53 +0800

    3.1 分页查询producer

--- cili_parent/service/service_pro/src/main/java/com/Jason/proservice/controller/ProducerController.java
+++ cili_parent/service/service_pro/src/main/java/com/Jason/proservice/controller/ProducerController.java
@@ -4,11 +4,13 @@ package com.Jason.proservice.controller;
 import com.Jason.common.utils.R;
 import com.Jason.proservice.entity.Producer;
 import com.Jason.proservice.service.ProducerService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.PrimitiveIterator;
 
 /**
  * <p>
@@ -33,7 +35,7 @@ public class ProducerController {
         return R.ok().data("items",AllProducerlist);
     }
     //根据id，逻辑删除讲师
-    @DeleteMapping("{id}")
+    @DeleteMapping("/delete/{id}")
     public R deleteByID(@PathVariable String id){
         System.out.println(id);
         boolean b = producerService.removeById(id);
@@ -43,6 +45,20 @@ public class ProducerController {
             return R.error();
         }
     }
+
+    @GetMapping("pageProducer/{current}/{limit}")
+    //current 当前页， limit每页记录数
+    public R pageListProducer(@PathVariable long current,@PathVariable long limit){
+        Page<Producer> pageProducer = new Page<>(current,limit);
+        //调用完方法后，所有分页数据都被封装到pageProducer对象里面
+        producerService.page(pageProducer,null);
+
+        long total = pageProducer.getTotal();//总记录数
+        List<Producer> records = pageProducer.getRecords();//数据list集合
+
+        return R.ok().data("total",total).data("rows",records);
+    }
+
 
 }
 
