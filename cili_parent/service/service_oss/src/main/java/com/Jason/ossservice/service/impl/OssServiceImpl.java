d297f90e88171da1540ec0c0a53d5463de97425f
Jas000n <ysyysy2001@126.com>
2023-03-16 11:47:36 +0800

    3.16 阿里云oss 1）给文件名新增uuid 2）文件按年月日新建文件夹进行管理

--- cili_parent/service/service_oss/src/main/java/com/Jason/ossservice/service/impl/OssServiceImpl.java
+++ cili_parent/service/service_oss/src/main/java/com/Jason/ossservice/service/impl/OssServiceImpl.java
@@ -4,11 +4,13 @@ import com.Jason.ossservice.service.OssService;
 import com.Jason.ossservice.utils.ConstPropertiesUtils;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
+import org.joda.time.DateTime;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.UUID;
 
 @Service
 public class OssServiceImpl implements OssService {
@@ -23,14 +25,21 @@ public class OssServiceImpl implements OssService {
         //创建oss对象，上传文件流
         OSS ossClient = new OSSClientBuilder().build(endpoint,accessKeyId,accessKeySecret);
 
-        System.out.println(file.getOriginalFilename());
-
+        //获取文件流
         InputStream inputStream = file.getInputStream();
-
+        //获取文件名
         String filename = file.getOriginalFilename();
 
-        ossClient.putObject(bucketName,filename,inputStream);
+        //在文件名中添加随机并且唯一的值，这样上传同文件名文件不会覆盖
+        String uuid = UUID.randomUUID().toString().replaceAll("-","");
+        filename = uuid+"_"+filename;
 
+        //把文件按照日期进行分类
+        String datePath = new DateTime().toString("yyyy/MM/dd/");
+        filename = datePath+filename;
+        //上传文件
+        ossClient.putObject(bucketName,filename,inputStream);
+        //关闭ossclient
         ossClient.shutdown();
 
         String url = "https://"+bucketName+"."+endpoint+"/"+filename;
