e1aada5cc7acc78d1e60e496ec27c4f42bf435d4
Jas000n <ysyysy2001@126.com>
2023-04-14 20:40:59 +0800

    4.14 更新sql和nginx配置

--- sql/sql.sql
+++ sql/sql.sql
@@ -34,7 +34,6 @@ create index idx_member_id
 create index idx_video_id
     on comment (episode_id);
 
-
 create table crm_banner
 (
     id           char(19)         default ''  not null comment 'ID'
@@ -77,6 +76,54 @@ create index idx_season_id
 
 create index idx_video_id
     on episode (video_id);
+
+create table oorder
+(
+    id            char(19)         default ''   not null
+        primary key,
+    order_no      varchar(20)      default ''   not null comment '订单号',
+    video_id      varchar(19)      default ''   not null comment '影视id',
+    video_title   varchar(100)                  null comment '影视名称',
+    video_cover   varchar(255)                  null comment '影视封面',
+    producer_name varchar(20)                   null comment '创作者名称',
+    member_id     varchar(19)      default ''   not null comment '会员id',
+    nickname      varchar(50)                   null comment '会员昵称',
+    mobile        varchar(11)                   null comment '会员手机',
+    total_fee     decimal(10, 2)   default 0.01 null comment '订单金额（分）',
+    pay_type      tinyint                       null comment '支付类型（1：微信 2：支付宝）',
+    status        tinyint                       null comment '订单状态（0：未支付 1：已支付）',
+    is_deleted    tinyint unsigned default '0'  not null comment '逻辑删除 1（true）已删除， 0（false）未删除',
+    gmt_create    datetime                      not null comment '创建时间',
+    gmt_modified  datetime                      not null comment '更新时间',
+    constraint ux_order_no
+        unique (order_no)
+)
+    comment '订单';
+
+create index idx_member_id
+    on oorder (member_id);
+
+create index idx_video_id
+    on oorder (video_id);
+
+create table pay_log
+(
+    id             char(19)         default ''   not null
+        primary key,
+    order_no       varchar(20)      default ''   not null comment '订单号',
+    pay_time       datetime                      null comment '支付完成时间',
+    total_fee      decimal(10, 2)   default 0.01 null comment '支付金额（分）',
+    transaction_id varchar(30)                   null comment '交易流水号',
+    trade_state    char(20)                      null comment '交易状态',
+    pay_type       tinyint          default 0    not null comment '支付类型（1：微信 2：支付宝）',
+    attr           text                          null comment '其他属性',
+    is_deleted     tinyint unsigned default '0'  not null comment '逻辑删除 1（true）已删除， 0（false）未删除',
+    gmt_create     datetime                      not null comment '创建时间',
+    gmt_modified   datetime                      not null comment '更新时间',
+    constraint uk_order_no
+        unique (order_no)
+)
+    comment '支付日志表';
 
 create table producer
 (
