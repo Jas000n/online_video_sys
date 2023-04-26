1baed51571c412dd7f4afc6ce7d01798478db2e4
Jas000n <ysyysy2001@126.com>
2023-04-26 23:08:35 +0800

    4.26 (1)完成了协同过滤,明天需要测试

---
+++ service-py-rec/generateREC.py
@@ -0,0 +1,97 @@
+import mv100
+import numpy as np
+def cos_sim(x, y):
+    '''余弦相似性
+    input:  x(mat):以行向量的形式存储，可以是用户或者商品
+            y(mat):以行向量的形式存储，可以是用户或者商品
+    output: x和y之间的余弦相似度
+    '''
+    numerator = x.dot(y)  # x和y之间的额积
+    denominator = (np.sqrt(x * x.T) * np.sqrt(y * y.T))[0]
+    return (numerator / denominator)
+
+
+def similarity(data):
+    '''计算矩阵中任意两行之间的相似度
+    input:  data(mat):任意矩阵
+    output: w(mat):任意两行之间的相似度
+    '''
+    data = data[1:][1:]
+    m = np.shape(data)[0]  # 用户的数量
+    # 初始化相似度矩阵
+    w = np.mat(np.zeros((m, m)))
+
+    for i in range(m):
+        for j in range(i, m):
+            if j != i:
+                # 计算任意两行之间的相似度
+                w[i, j] = cos_sim(data[i,], data[j,])
+                w[j, i] = w[i, j]
+            else:
+                w[i, j] = 0
+    return w
+
+
+def user_based_recommend(data, w, user):
+    '''基于用户相似性为用户user推荐商品, *******注意这里的user是id*********
+    input:  data(mat):用户商品矩阵
+            w(mat):用户之间的相似度
+            user(int):用户的编号
+    output: predict(list):推荐列表
+    '''
+    user_list = data[0][:]
+    video_list = data[:][0]
+    data = data[1:][1:]
+
+    user = list(user_list).index(user)
+
+    m, n = np.shape(data)
+    interaction = data[user,]  # 用户user与商品信息
+
+    # 1、找到用户user没有互动过的商品
+    not_inter = []
+    for i in range(n):
+        if interaction[i] == 0:  # 没有互动的商品
+            not_inter.append(i)
+
+    # 2、对没有互动过的商品进行预测
+    predict = {}
+    for x in not_inter:
+        item = np.copy(data[:, x])  # 找到所有用户对商品x的互动信息
+        for i in range(m):  # 对每一个用户
+            if item[i] != 0:  # 若该用户对商品x有过互动
+                if x not in predict:
+                    predict[x] = w[user, i] * item[i]
+                else:
+                    predict[x] = predict[x] + w[user, i] * item[i]
+    # 3、按照预测的大小从大到小排序
+    return sorted(predict.items(), key=lambda d: d[1], reverse=True)
+
+def top_k(predict, k):
+    '''为用户推荐前k个商品
+    input:  predict(list):排好序的商品列表
+            k(int):推荐的商品个数
+    output: top_recom(list):top_k个商品
+    '''
+    top_recom = []
+    len_result = len(predict)
+    if k >= len_result:
+        top_recom = predict
+    else:
+        for i in range(k):
+            top_recom.append(predict[i])
+    return top_recom
+
+if __name__ == '__main__':
+    # 1、导入用户商品数据
+    print("------------ 1. 加载数据 ------------")
+    data = mv100.generateMatrix("./test.csv")
+    print("------------ 2. 计算两个用户间的相似度-------------")
+    w = similarity(data)
+    # 3、利用用户之间的相似性进行推荐
+    print("------------ 3. 预测用户与没有交互过得产品的喜爱程度 ------------")
+    predict = user_based_recommend(data, w, 1)
+    # 4、进行Top-K推荐
+    print("------------ 4. 推荐k个产品 ------------")
+    top_recom = top_k(predict, 2)
+    print(top_recom)
---
+++ service-py-rec/mv100.py
@@ -0,0 +1,47 @@
+import  numpy as np
+import pandas as pd
+def generateMatrix(filename):
+
+    #获得numpy array
+    csvFile = np.genfromtxt(filename,delimiter=",")
+    print(csvFile.shape)
+
+
+    #去除time stamp和头行信息
+    csvFile = csvFile[1:,-2:]
+    print(csvFile.shape)
+
+
+    #获得应该构建的矩阵大小
+    pdFrame = pd.DataFrame(csvFile)
+    print(pdFrame)
+    user_num = pdFrame.value_counts(0).count()
+    video_num = pdFrame.value_counts(1).count()
+
+    #先构建空白矩阵
+    matrix = np.zeros((user_num+1,video_num+1),dtype=int)
+    print(matrix.shape)
+
+    #获得user list和video list, 此时不含重复元素
+    userList = csvFile[:,0].tolist()
+    userList = list(set(userList))
+    videoList = csvFile[:,1].tolist()
+    videoList = list(set(videoList))
+
+    #给空白矩阵赋值,此时先把左,上的标签写上
+    for i in range(0, len(userList)):
+        matrix[i+1][0] = int(userList[i])
+    for i in range(0,len(videoList)):
+        matrix[0][i+1] = int(videoList[i])
+
+    #给空白矩阵赋值,
+    for i in range(0,csvFile.shape[0]):
+        user = int(csvFile[i][0])
+        video = int(csvFile[i][1])
+        user_index = userList.index(user) +1
+        video_index = videoList.index(video)+1
+        matrix[user_index][video_index] = 1
+    print(matrix)
+    return matrix
+if __name__ == '__main__':
+    generateMatrix("./test.csv")
